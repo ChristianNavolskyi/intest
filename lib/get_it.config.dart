6b671ba890d56adb5cf3452034bb7b196016f8ab
Christian Navolskyi <christian.navolskyi@arconsis.com>
2021-03-01 11:14:12 +0100

    updated versions

--- lib/get_it.config.dart
+++ lib/get_it.config.dart
@@ -4,25 +4,19 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
 
-import 'a.dart';
-import 'util.dart';
-import 'b.dart' as intest;
+import 'a.dart' as _i3;
+import 'b.dart' as _i5;
+import 'util.dart' as _i4; // ignore_for_file: unnecessary_lambdas
 
-/// adds generated dependencies
-/// to the provided [GetIt] instance
-
-GetIt $initGetIt(
-  GetIt get, {
-  String environment,
-  EnvironmentFilter environmentFilter,
-}) {
-  final gh = GetItHelper(get, environment, environmentFilter);
-
-  // Eager singletons must be registered in the right order
-  gh.singleton<A>(AImpl(get<B>()));
-  gh.singleton<intest.B>(intest.BImpl());
+// ignore_for_file: lines_longer_than_80_chars
+/// initializes the registration of provided dependencies inside of [GetIt]
+_i1.GetIt $initGetIt(_i1.GetIt get,
+    {String environment, _i2.EnvironmentFilter environmentFilter}) {
+  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  gh.singleton<_i3.A>(_i3.AImpl(get<_i4.B>()));
+  gh.singleton<_i5.B>(_i5.BImpl());
   return get;
 }
